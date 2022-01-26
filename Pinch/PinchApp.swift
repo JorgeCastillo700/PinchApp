0f194756bec407bb43835aacb742018ebe9dbef2
JCastillo <jorge.castillo.700@gmail.com>
2022-01-25 23:28:01 -0800

    upload project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jorge Castillo on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
